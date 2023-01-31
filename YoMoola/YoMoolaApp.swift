ccb72d371cd74886fe3c77325859b2b3d2c2f1fb
Ilbum Kwak <ilbum@nbranchconsulting.com>
2023-01-30 17:13:31 -0800

    Initial Commit

---
+++ YoMoola/YoMoolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoMoolaApp.swift
+//  YoMoola
+//
+//  Created by Blue Heron on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct YoMoolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
